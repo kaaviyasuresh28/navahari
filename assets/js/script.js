fd8ff98d1ebf2a817adc807bc14530e4c93318af
Kaaviya Suresh <123892088+kaaviyasuresh28@users.noreply.github.com>
2025-05-14 21:23:56 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+
+
+
+
+
+
+
+/*NAVIGATION BAR*/
+
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+
+/* Menu show */
+if(navToggle){
+   navToggle.addEventListener('click', () =>{
+      navMenu.classList.add('show-menu')
+   })
+}
+
+/* Menu hidden */
+if(navClose){
+   navClose.addEventListener('click', () =>{
+      navMenu.classList.remove('show-menu')
+   })
+}
+
+
+if(navMenu){
+   navMenu.addEventListener('click', () => {
+      navMenu.classList.remove('show-menu')
+   })
+}
+
+
+
+
+
+
+
+
+
+
+
+/* Contact Form*/
